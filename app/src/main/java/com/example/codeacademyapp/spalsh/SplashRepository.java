dc6dbb4d9669e297973bf1ec674c5d54be5472e6
dejANdroid <figaroakomoze86@gmail.com>
2020-01-25 01:05:10 +0100

    commit_changes

--- app/src/main/java/com/example/codeacademyapp/spalsh/SplashRepository.java
+++ app/src/main/java/com/example/codeacademyapp/spalsh/SplashRepository.java
@@ -3,13 +3,11 @@ package com.example.codeacademyapp.spalsh;
 import android.os.Handler;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.lifecycle.MutableLiveData;
 
 import com.example.codeacademyapp.users.model.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,18 +15,15 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.Objects;
-
 public class SplashRepository {
 
-    private FirebaseAuth firebaseAuth;
     private MutableLiveData<FirebaseUser> isUserAuthenticateInFirebaseMutableLiveData;
     private MutableLiveData<User> getChild;
     private FirebaseUser firebaseUser;
 
-    public SplashRepository() {
+    SplashRepository() {
         isUserAuthenticateInFirebaseMutableLiveData = new MutableLiveData<>();
-        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
     }
 
@@ -44,19 +39,19 @@ public class SplashRepository {
         return isUserAuthenticateInFirebaseMutableLiveData;
     }
 
-    MutableLiveData<User> getChil (){
-       FirebaseDatabase database= FirebaseDatabase.getInstance();
-       getChild=new MutableLiveData<>();
+    MutableLiveData<User> getChil() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        getChild = new MutableLiveData<>();
 
         final DatabaseReference userRef = database.getReference();
 
-        Query query= userRef.child("Users").orderByChild("Name").equalTo("");
+        Query query = userRef.child("Users").orderByChild("Name").equalTo("");
 
         query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                User user=dataSnapshot.getValue(User.class);
+                User user = dataSnapshot.getValue(User.class);
                 getChild.setValue(user);
             }
 
