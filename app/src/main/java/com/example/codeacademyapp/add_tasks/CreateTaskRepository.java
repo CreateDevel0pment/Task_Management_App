1a078974acddb58277082973acda5276fbcaca9c
Kosevski <kosevski.a@gmiaol.com>
2020-01-26 18:44:38 +0100

    Merge branch 'master' of C:\Users\Kosevski\Task_Management_App with conflicts.

--- app/src/main/java/com/example/codeacademyapp/add_tasks/CreateTaskRepository.java
+++ app/src/main/java/com/example/codeacademyapp/add_tasks/CreateTaskRepository.java
@@ -1,8 +1,10 @@
 package com.example.codeacademyapp.add_tasks;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 
 import com.example.codeacademyapp.model.Task;
+import com.example.codeacademyapp.model.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -15,27 +17,33 @@ public class CreateTaskRepository {
     private FirebaseAuth mAuth;
     private DatabaseReference myRef;
 
+
     public CreateTaskRepository() {
         setTaskInformation = new MutableLiveData<>();
         mAuth = FirebaseAuth.getInstance();
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        myRef = firebaseDatabase.getReference();
+
     }
 
     public MutableLiveData<Task> createNewTask(Task task) {
 
+
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = firebaseDatabase.getReference();
+
         if (!task.getName().equals("") && !task.getDescription().equals("") &&
                 !task.getNote().equals("")) {
+
 
         FirebaseUser userFb = mAuth.getCurrentUser();
         String userID;
         if (userFb != null) {
             userID = userFb.getUid();
 
-            myRef.child("Tasks").child(userID).child(task.getName()).child("Task Information")
+            myRef.child("TaskGroups").child(task.getGroup()).child(task.getName())
                     .child("Description")
                     .setValue(task.getDescription());
-            myRef.child("Tasks").child(userID).child(task.getName()).child("Task Information")
+            myRef.child("TaskGroups").child(task.getGroup()).child(task.getName())
                     .child("Task Note")
                     .setValue(task.getNote());
 //            myRef.child("Tasks").child(task.getId()).child("Task Information")
@@ -50,13 +58,16 @@ public class CreateTaskRepository {
 //            myRef.child("Tasks").child(task.getId()).child("Task Information")
 //                    .child("Task Start Date")
 //                    .setValue(task.getStart_date());
-            myRef.child("Tasks").child("Task Information")
-                    .child("Creator")
-                    .setValue(userID);
+//            myRef.child("Tasks").child("Task Information")
+//                    .child("Creator")
+//                    .setValue(userID);
         }
         }
         return setTaskInformation;
     }
+
+
+
 }
 
 
