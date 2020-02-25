a7fff81a4649978a8d417cb0e9cf070f7984b3af
dejANdroid <figaroakomoze86@gmail.com>
2020-02-25 16:50:55 +0100

    layout_changes

--- app/src/main/java/com/example/codeacademyapp/ui/main/sector/task/TaskActivity.java
+++ app/src/main/java/com/example/codeacademyapp/ui/main/sector/task/TaskActivity.java
@@ -7,11 +7,14 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.codeacademyapp.R;
 import com.example.codeacademyapp.ui.main.sector.task.fragment.AddNewTaskFragment;
 import com.example.codeacademyapp.ui.main.sector.task.fragment.TaskTabsFragment;
+import com.example.codeacademyapp.utils.NetworkConnectivity;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -70,6 +73,19 @@ public class TaskActivity extends AppCompatActivity {
             manager.beginTransaction()
                     .replace(R.id.task_fragments_container, taskTabsFragment)
                     .commit();
+        }
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (NetworkConnectivity.isConnectivityNetworkAvailable(this)) {
+
+            Toast connectivityToast = Toast.makeText(this, "No Internet Connection", Toast.LENGTH_LONG);
+            connectivityToast.setGravity(Gravity.CENTER, 0, 0);
+            connectivityToast.show();
         }
     }
 }
